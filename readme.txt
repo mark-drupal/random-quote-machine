81b64a8447af1873d28eb1cdb9a43f9d2bfb0949
mark newton <mark.drupal@gmail.com>
2020-12-28 15:17:52 +0000

    added a readme.txt

---
+++ readme.txt
@@ -0,0 +1,21 @@
+
+***********************************
+*                                 *
+*       Random Quote Machine	  *
+*				  *
+***********************************
+
+This Forms part of the freecodecamp tutorials on front end libaries.  The tutorials at freecodecamp can be found here:
+
+https://www.freecodecamp.org/learn/
+
+As a part of the tutorials I learnt about Bootstrap, JQuery, Sass, React, and Redux.  
+
+The random quote machine was a task that I was set to complete.  You can view a solution of what it should look like in order to pass
+here:
+
+https://codepen.io/freeCodeCamp/full/qRZeGZ
+
+I decided initially instead of forking the codepen.io test, that I would gain more out of coding myself on my local enviroment.
+
+
--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
+/*
+ The Quote Component displays just a quote
+*/
+
+class Quote extends React.Component {
+  constructor() {
+    super();
+
+
+    }
+
+
+  render() {
+    return (
+      <div>
+        <p id="quote-text">test</p>
+        <p id="quote-author">author</p>
+        <button
+          id="new-quote"
+          onClick=""
+          >
+          New quote
+
+        </button>
+
+      </div>
+    );
+  }
+
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Quote />
       </header>
     </div>
   );
