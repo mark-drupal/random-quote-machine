9e713dbc7d782db3631e257e0fa7e487e203ae0b
mark newton <mark.drupal@gmail.com>
2020-12-29 16:41:53 +0000

    I think that this works a bit better now.  It displays a random quote when the page is loaded now as well as providing a random quote when the new quote button is pressed.  I have achieved this by seperating out the code a little and created quote.js.  This component handles creating a new quote.  Essentially its just an array of quotes which are then randomly selected.  I feel that this is a better way of using Reacts modularlity and makes things a bit simpler as well as protecting functionalty and data better

--- src/App.js
+++ src/App.js
@@ -1,70 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Quote from './Quote.js';
 
 /*
  The Quote Component displays just a quote
 */
 
-class Quote extends React.Component {
-
-  /*This function selects a random quote*/
-  updateQuote = () => {
-
-    /*This array contains our quotes*/
-    let quotes = [
-      {
-        "quote":"over the line",
-        "author":"walter"
-      },
-      {
-        "quote":"she probably kid napped herself",
-        "author":"The Dude"
-      },
-      {
-        "quote":"I am staying, I'm drinking my coffee",
-        "author":"Walter"
-      }
-    ];
-
-    /*select a random quote*/
-    let selectedQuote = quotes[Math.floor(Math.random()*quotes.length)];
-    /*update the state of the quote and author*/
-    this.setState({quote:selectedQuote.quote});
-    this.setState({author:selectedQuote.author});
-  }
+class FullQuote extends React.Component {
 
   constructor() {
     super();
     /*Set up the quote and author in the state object*/
-    this.state = {
-        quote: this.updateQuote.quote,
-        author: this.updateQuote.author
-      };
-    }
+  }
 
-    render() {
-      return (
-        <div>
-          {/*As per the requirements for the use case this tage is the quote text*/}
-          <p id="quote-text">{this.state.quote}</p>
-          {/*As per the requirements for the use case this tag for the author*/}
-          <p id="quote-author">{this.state.author}</p>
-          {/*This is the button which brings back a new quote*/}
-          <button
+  /*This arrow function forces the update*/
+  updateQuote = () => {
+    this.forceUpdate();
+  }
 
-            id="new-quote"
-            
-            onClick={this.updateQuote}>
-            New quote
-
-          </button>
-
+  render() {
+    return (
+      <div>
+        {/*The brings in a new quote component from ./Quote.js*/}
+        <Quote />
+        {/*This is the button that get a new quote calling this.updateQuote*/}
+        <button
+          id="new-quote" onClick={this.updateQuote}>
+          New quote
+        </button>
       </div>
     );
   }
-
+  
 }
 
 
@@ -73,7 +41,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Quote />
+        <FullQuote />
       </header>
     </div>
   );
---
+++ src/Quote.js
@@ -0,0 +1,41 @@
+/*Import the react libary to extend off*/
+import React from 'react'
+
+/*The quote class will handle creating a new quote*/
+class Quote extends React.Component {
+    render (){
+      /*This array contains our quotes*/
+     let quotes = [
+      {
+        "quote":"over the line",
+        "author":"walter"
+      },
+      {
+        "quote":"she probably kid napped herself",
+        "author":"The Dude"
+      },
+      {
+        "quote":"I am staying, I'm drinking my coffee",
+        "author":"Walter"
+      }
+    ];
+
+    /*select a random quote*/
+    let selectedQuote = quotes[Math.floor(Math.random()*quotes.length)];
+    /*Transfer the selected quote into the relevant states */
+    this.state ={
+      author: selectedQuote.author,
+      quote: selectedQuote.quote
+    }
+    /*return the selected quote*/
+    return (
+      <div id="quote">
+        <div id="text">{this.state.quote}</div>
+        <div id="author">{this.state.author}</div>
+      </div>
+    );
+  }
+
+}
+
+export default Quote;
